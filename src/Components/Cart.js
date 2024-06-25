f720158d088dc0aaa4caf9e1d8e9e741d16c7a00
dharm <dpp6402@gmail.com>
2024-06-25 13:51:49 +0530

    add loader

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,8 +1,10 @@
 import React from "react";
+import TruckLoader from "./DotLoader";
 
 export default function Cart() {
   return (
-    <div>
+    <div className="text-center">
+      <TruckLoader />
       <h1 className="text-center">cart is Emptey</h1>
     </div>
   );
