b1a537da6c3fbf8f02f9ebbdbbcc52ec0bdd3cc8
dharm <dpp6402@gmail.com>
2024-07-17 21:29:42 +0530

    product_details

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,7 +1,7 @@
-// ProductDetails.js
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "./ProductDetail.css"; // Ensure the CSS file name is correct
 
 function ProductDetails() {
   const { productId } = useParams();
@@ -13,6 +13,7 @@ function ProductDetails() {
     axios
       .get(`https://fakestoreapi.com/products/${productId}`)
       .then((res) => {
+        console.log(res.data.image); // Log image URL for debugging
         setProduct(res.data);
         setLoading(false);
       })
@@ -28,10 +29,53 @@ function ProductDetails() {
 
   return (
     <div className="container">
-      <h2>{product.title}</h2>
-      <img src={product.image} alt={product.title} />
-      <p>{product.description}</p>
-      <p>${product.price}</p>
+      <div className="card">
+        <div className="container-fluid">
+          <div className="wrapper row">
+            <div className="preview col-md-6">
+              <img
+                src={product.image}
+                className="card-img-top"
+                alt={product.title}
+              />
+              <div className="preview-pic tab-content">
+                <div className="tab-pane active" id="pic-1">
+                </div>
+              </div>
+            </div>
+            <div className="details col-md-6">
+              <h3 className="product-title">{product.title}</h3>
+              <div className="rating">
+                <div className="stars">
+                  {[...Array(5)].map((_, i) => (
+                    <span
+                      key={i}
+                      className={`fa fa-star ${
+                        i < Math.round(product.rating.rate) ? "checked" : ""
+                      }`}
+                    ></span>
+                  ))}
+                </div>
+                <span className="review-no">
+                  {product.rating.count} reviews
+                </span>
+              </div>
+              <p className="product-description">{product.description}</p>
+              <h4 className="price">
+                current price: <span>${product.price}</span>
+              </h4>
+              <div className="action">
+                <button className="add-to-cart btn btn-default" type="button">
+                  add to cart
+                </button>
+                <button className="like btn btn-default" type="button">
+                  <span className="fa fa-heart"></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
